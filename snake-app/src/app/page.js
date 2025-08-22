ba900daf8997d5c51cfa22b2be6b1793c71e09a9
Bi0informatic <147007881+Bi0informatic@users.noreply.github.com>
2025-08-22 00:19:57 -0700

    added App component

--- snake-app/src/app/page.js
+++ snake-app/src/app/page.js
@@ -2,14 +2,16 @@ import GameBoard from "./components/GameBoard.js";
 import RestartButton from "./components/RestartButton.js";
 import DifficultyButtons from "./components/DifficultyButtons.js";
 import ScoreBoard from "./components/ScoreBoard.js";
+import App from "./components/App.js";
 
 export default function Home() {
   return (
     <div id="container">
-        <ScoreBoard/>
-        <GameBoard/>
-        <RestartButton/>
-        <DifficultyButtons/>
+      <App/>
+      <ScoreBoard/>
+      <GameBoard/>
+      <RestartButton/>
+      <DifficultyButtons/>
     </div>
   );
 }
