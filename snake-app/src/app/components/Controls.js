8d5fe4848fc9ceedb01069434668e593ecffec11
Bi0informatic <147007881+Bi0informatic@users.noreply.github.com>
2025-08-22 23:54:11 -0700

    implemented Controls

--- snake-app/src/app/components/Controls.js
+++ snake-app/src/app/components/Controls.js
@@ -1,8 +1,8 @@
-export default function Controls() {
+export default function Controls({onRestart, onSetSpeed}) {
     return (<div id="control-buttons">
-        <button id="restart-button" className="button">Restart</button>
-        <button id="easy-button" className="difficulty button">Easy</button>
-        <button id="normal-button" className="difficulty button">Normal</button>
-        <button id="hard-button" className="difficulty button">Hard</button>
+        <button id="restart-button" className="button" onclick={onRestart}>Restart</button>
+        <button id="easy-button" className="difficulty button" onclick={()=>{onSetSpeed(80); onRestart();}}>Easy</button>
+        <button id="normal-button" className="difficulty button" onclick={()=>{onSetSpeed(90); onRestart();}}>Normal</button>
+        <button id="hard-button" className="difficulty button" onclick={()=>{onSetSpeed(100); onRestart();}}>Hard</button>
     </div>)
 }
